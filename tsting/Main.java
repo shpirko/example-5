27b2c0249b7b6c5f9e4376aaa80abdabafd0ed6c
Shaked.Shpirko <shaked9904@gmail.com>
2025-04-20 10:39:46 +0300

    new function

--- tsting/Main.java
+++ tsting/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+    } 
+    public static void test() {
+        System.out.println("Test method called!");
+    } 
 
 }
